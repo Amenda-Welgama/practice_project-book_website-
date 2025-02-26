5417772e4c43db4877808a46c757990b989c9b98
Amenda-Welgama <amenda20020506@gmail.com>
2025-02-26 09:02:06 +0530

    Added practice project website files - 2/26/2025
    This commit includes the foundational files for my practice project website, 
    which is built using HTML, CSS, JavaScript, and PHP. The goal of this project 
    is to enhance my web development skills and practice backend functionality 
    with PHP while using GitHub for version control.

---
+++ header.php
@@ -0,0 +1,18 @@
+<header class="header">
+    <div class="header-1">
+        <div class="flex">
+            <div class="share">
+                <a href="" class="fab fa-facebook-f"></a>
+                <a href="" class="fab fa-instagram"></a>
+                <a href="" class="fab fa-linkedin"></a>
+                <a href="" class="fab fa-twitter"></a>
+            </div>
+            <p>new <a href="loging.php">Loging</a>|<a href="register.php">Register</a></p>
+        </div>
+    </div>
+    <div class="header-2">
+        <a href="home.php" class="logo">Home</a>
+
+        
+    </div>
+</header>
